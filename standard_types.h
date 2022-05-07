37b8974e2c27cb046526ad752c1de4faae08ce5d
ZeyadMohamed20 <zeyadmohamedasu@gmail.com>
2022-05-07 02:27:42 +0200

    Add Standard Integral Types File

---
+++ standard_types.h
@@ -0,0 +1,16 @@
+#ifndef STANDARD_TYPES_H
+#define STANDARD_TYPES_H
+typedef long int int32_t
+/* exact-width signed integer types */
+typedef   signed           char int8_t;
+typedef   signed short     int  int16_t;
+typedef   signed           int  int32_t;
+typedef   signed       __int64 int64_t;
+
+    /* exact-width unsigned integer types */
+typedef unsigned          char uint8_t;
+typedef unsigned short     int uint16_t;
+typedef unsigned           int uint32_t;
+typedef unsigned       __int64 uint64_t;
+ 
+#endif
