9e2ccfe5c71b8a60bc7001e84f71b4ea49492004
Hassan Khaled <hassankhaled006@gmail.com>
2022-05-07 05:40:42 +0200

    Create LCD.h file

---
+++ LCD.h
@@ -0,0 +1,17 @@
+#include"G:/Labware/inc/tm4c123gh6pm.h"
+#include"standard_types.h"
+
+//********** Functions Prototype ***************
+void SystemInit(void);
+void timer(uint32_t milliSeconds);
+void delay(uint32_t milliSeconds);
+void lcd_cmd(char command);
+void lcd_data(char data);
+void lcd_init(void);
+void lcd_clear(void);
+void lcd_secline(void);
+void lcd_display(char *str);
+void lcd_setposition(uint16_t row,uint16_t col);
+
+//**********************************************
+
