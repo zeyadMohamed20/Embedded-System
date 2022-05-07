72a7e829f9bd388583a4439cc89b76fd6a8589ce
Zeyad Mohamed <zeyadmohamedasu@gmail.com>
2022-05-07 06:12:11 +0200

    Create macros.h file

---
+++ macros.h
@@ -0,0 +1,10 @@
+/*
+ ***********************************************************************************************************
+@file     standard_types.h
+@brief    This file rename the satndard data types in c by adding the size to thir names
+@version  V1.00
+@date     7. May 2022
+@team     Integration Team
+Copyright (C) 2022. All rights reserved.
+*************************************************************************************************************
+*/
