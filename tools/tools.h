3d02a4b6ac8eef18695dfd0b1b7c651690eed708
B578-design <104259146+B578-design@users.noreply.github.com>
2022-05-12 16:32:05 +0200

    Update tools.h

--- tools/tools.h
+++ tools/tools.h
@@ -7,14 +7,14 @@
 //leds f1 f2 f3//
 //SW2 F0//
 //SW1 F4//
-void ENABLE_PORTF_AND_A();//enable ports f and port a
-void PORTF(); //initiallizing port f
-void PORTA();//initiallizing port a
+void enable_portf_and_a();//enable ports f and port a
+void portf(void); //initiallizing port f
+void porta(void);//initiallizing port a
 void GPIOF_Handler(void); //interrupt SW1 , SW2//
 void GPIOA_Handler(void); //interrupt push button//
-void Leds_On();
-void Leds_Off();
-void Buzzer_On();
-void Buzeer_off();	
-void Leds_Blink(uint8_t NumberOfBlink,uint8_t OnDelay,uint32_t delayTimeOn,uint8_t OffDelay,uint32_t delayTimeOff);
+void leds_on();
+void leds_off();
+void buzzer_on();
+void buzeer_off();	
+void leds_blink(uint8_t NumberOfBlink,uint8_t OnDelay,uint32_t delayTimeOn,uint8_t OffDelay,uint32_t delayTimeOff);
 #endif // !TOOLS_H
