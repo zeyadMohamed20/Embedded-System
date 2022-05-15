3dccc7ea207c27ca733a7ab4ce9b17614f8de83c
B578-design <104259146+B578-design@users.noreply.github.com>
2022-05-15 17:50:05 +0200

    Update tools.c

--- tools/tools.c
+++ tools/tools.c
@@ -24,7 +24,6 @@ GPIO_PORTF_CR_R=0x1F;
 	GPIO_PORTF_DIR_R=0x1E;
 	GPIO_PORTF_DEN_R=0x1F;
 	GPIO_PORTF_PUR_R=0x11;
-	GPIO_PORTF_ICR_R |= (1<<4)|(1<<0);
 }
 //initiallization port a	
 void porta()
@@ -37,41 +36,37 @@ GPIO_PORTA_CR_R |= 0x1F;
 	GPIO_PORTA_DIR_R|=0x1E;
 	GPIO_PORTA_DEN_R|=0x1F;
 	GPIO_PORTA_PUR_R|=0x11;
-	GPIO_PORTA_ICR_R |= (1<<2);
 }
 //usage of Push button as Interrupt//
 
 /*void GPIOA_Handler(void)
-{  if ((GPIO_PORTA_MIS_R&0x04)!=0x04)
+{  if ((GPIO_PORTA_DATA_R&0x04)!=0x04)
 	pause();
-	GPIO_PORTA_ICR_R  |= 0x04;
 }*/
 
 //usage of SW1 and SW2 buttons as interrupt//
 void GPIOF_Handler(void){
 	   //SW2 is pushed for 1st time and start cooking and leds turn on//
-      if ((GPIO_PORTF_MIS_R &0x11)==0x01)
+      if ((GPIO_PORTF_DATA_R &0x11)==0x10)
 	      {
 					leds_on();	
 					//SW1 is pushed for 1st time and pause cooking//
-					if ((GPIO_PORTF_MIS_R &0X11)==0X11)
-					{
+			if ((GPIO_PORTF_DATA_R &0X11)==0X00)
+				{
 						pause();
-						//SW1 is pushed for 2nd time and stop cooking and leds turn off//
-					if((GPIO_PORTF_MIS_R & 0x11)==0x01)
-        {
+					//SW1 is pushed for 2nd time and stop cooking and leds turn off//
+					if((GPIO_PORTF_DATA_R & 0x11)==0x10)
+                                             {
 					lcd_clear();
 					leds_off();	
-					GPIO_PORTF_ICR_R  |= (1<<0);
-        }
-				//SW2 is pushed for 2nd time after 1st time of SW1 is pushed which resume cooking after pause cooking//
-				else if((GPIO_PORTF_MIS_R &0x11)==0x10)
-                {
+                                              }
+				        //SW2 is pushed for 2nd time after 1st time of SW1 is pushed which resume cooking after pause cooking//
+				        else if((GPIO_PORTF_DATA_R &0x11)==0x01)
+                                                                {
 									resume();
 									leds_on();
-									GPIO_PORTF_ICR_R |= (1<<4);
 								}
-			}
+			                    }
 		}
 	}
 //turn on leds//
@@ -87,7 +82,7 @@ void leds_off()
 //turn on buzzer//
 void buzzer_on()
 {
-	GPIO_PORTA_DATA_R=0x08;
+	GPIO_PORTA_DATA_R=0x0C;
 }
 //turn off buzzer//
 void buzzer_off()
