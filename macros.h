bd4d62e7651e418475d606e0a18ed06be35717bb
Bishoy Yousry <1900733@eng.asu.edu.eg>
2022-05-17 21:14:48 +0200

    Edit set_time, display_time

--- macros.h
+++ macros.h
@@ -112,8 +112,6 @@ Copyright (C) 2022. All rights reserved.
 //********************************GPIO registers (PORTF)*********************************************************
 #define GPIO_PORTF_DATA_R       (*((volatile uint32_t *)0x400253FC))
 #define GPIO_PORTF_DIR_R        (*((volatile uint32_t *)0x40025400))
-#define GPIO_PORTF_MIS_R        (*((volatile uint32_t *)0x40025418))
-#define GPIO_PORTF_ICR_R        (*((volatile uint32_t *)0x4002541C))
 #define GPIO_PORTF_AFSEL_R      (*((volatile uint32_t *)0x40025420))
 #define GPIO_PORTF_PUR_R        (*((volatile uint32_t *)0x40025510))
 #define GPIO_PORTF_DEN_R        (*((volatile uint32_t *)0x4002551C))
@@ -121,6 +119,13 @@ Copyright (C) 2022. All rights reserved.
 #define GPIO_PORTF_CR_R         (*((volatile uint32_t *)0x40025524))
 #define GPIO_PORTF_AMSEL_R      (*((volatile uint32_t *)0x40025528))
 #define GPIO_PORTF_PCTL_R       (*((volatile uint32_t *)0x4002552C))
+#define GPIO_PORTF_IS_R         (*((volatile uint32_t *)0x40025404))
+#define GPIO_PORTF_IBE_R        (*((volatile uint32_t *)0x40025408))
+#define GPIO_PORTF_IEV_R        (*((volatile uint32_t *)0x4002540C))
+#define GPIO_PORTF_ICR_R        (*((volatile uint32_t *)0x4002541C))
+#define GPIO_PORTF_IM_R         (*((volatile uint32_t *)0x40025410))
+#define GPIO_PORTF_MIS_R        (*((volatile uint32_t *)0x40025418))
+#define NVIC_EN0_R              (*((volatile uint32_t *)0xE000E100))
 //*****************************************************************************************************************
 
 //***********************************GPIO Constant Macros******************************************************
